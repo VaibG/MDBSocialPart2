788d41972e4f4d600449c46b5430215b2ad1c83b
Vikas 🏆 Gattani <Vaibhav@vaibhavs-macbookpro.local>
2018-10-05 15:32:11 -0700

    Initial Commit

---
+++ MDBSocialPart2Tests/MDBSocialPart2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MDBSocialPart2Tests.swift
+//  MDBSocialPart2Tests
+//
+//  Created by Vaibhav Gattani on 5/10/18.
+//  Copyright © 2018 Vaibhav Gattani. All rights reserved.
+//
+
+import XCTest
+@testable import MDBSocialPart2
+
+class MDBSocialPart2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
